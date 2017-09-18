f1b6670fcc0f1b06ff1f528a917ac05c5baf6cea
Jakub Toczek <jakub.toczek@man.poznan.pl>
2017-09-18 12:38:48 +0200

    FederationRegistrationHelper with test.

--- src/main/java/helpers/FederationRegistrationHelper.java
+++ src/main/java/helpers/FederationRegistrationHelper.java
@@ -1,5 +1,6 @@
 package helpers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -9,19 +10,21 @@ import eu.h2020.symbiote.security.communication.payloads.FederationRule;
 import eu.h2020.symbiote.security.communication.payloads.FederationRuleManagementRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeoutException;
 
 public class FederationRegistrationHelper {
 
     private static Log log = LogFactory.getLog(FederationRegistrationHelper.class);
-    protected static ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //TODO: fill all the proper fields
         String federationId = "";
         Set<String> platformsIds = new HashSet<>();
         platformsIds.add("");
@@ -34,40 +37,60 @@ public class FederationRegistrationHelper {
         String rabbitPassword = "";
         String federationRuleManagementRequestQueue = "";
 
-        Connection connection = null;
-        RpcClient federationRuleManagementOverAMQPClient = null;
-        try {
-             connection = getConnection(rabbitHost,rabbitUsername,rabbitPassword);
-             federationRuleManagementOverAMQPClient = new RpcClient(connection.createChannel(), "",
-                    federationRuleManagementRequestQueue, 5000);
-        } catch (IOException | TimeoutException e) {
-            log.error("Failed to open connection.");
-        }
-
-
-        FederationRuleManagementRequest federationRuleManagementRequest = new FederationRuleManagementRequest(
-                new Credentials(AAMOwnerUsername, AAMOwnerPassword),
-                federationId,
-                platformsIds,
-                FederationRuleManagementRequest.OperationType.CREATE);
 
         try {
-            byte[] response = federationRuleManagementOverAMQPClient.primitiveCall(mapper.writeValueAsString
-                    (federationRuleManagementRequest).getBytes());
+            registerFederation(
+                    federationId,
+                    platformsIds,
+                    AAMOwnerUsername,
+                    AAMOwnerPassword,
+                    rabbitHost,
+                    rabbitUsername,
+                    rabbitPassword,
+                    federationRuleManagementRequestQueue);
+            log.info("OK");
         } catch (IOException | TimeoutException e) {
-            log.error("Fail during communication occured.");
+            log.error(e.getMessage());
+            log.error(e.getCause());
         }
-
 
 
     }
 
-    public static Connection getConnection(String rabbitHost, String rabbitUsername, String rabbitPassword) throws IOException, TimeoutException {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost(rabbitHost);
-            factory.setUsername(rabbitUsername);
-            factory.setPassword(rabbitPassword);
-            return factory.newConnection();
+    public static Map<String, FederationRule> registerFederation(String federationId, Set<String> platformsIds, String aamOwnerUsername, String aamOwnerPassword, String rabbitHost, String rabbitUsername, String rabbitPassword, String federationRuleManagementRequestQueue) throws IOException, TimeoutException {
+        Connection connection = getConnection(rabbitHost, rabbitUsername, rabbitPassword);
+        RpcClient federationRuleManagementOverAMQPClient = new RpcClient(connection.createChannel(), "",
+                federationRuleManagementRequestQueue, 5000);
+
+        log.info("Connection over AMQP established.");
+        FederationRuleManagementRequest federationRuleManagementRequest = new FederationRuleManagementRequest(
+                new Credentials(aamOwnerUsername, aamOwnerPassword),
+                federationId,
+                platformsIds,
+                FederationRuleManagementRequest.OperationType.CREATE);
+        log.info("FederationRuleManagementRequest constructed.");
+
+        byte[] response = federationRuleManagementOverAMQPClient.primitiveCall(mapper.writeValueAsString
+                (federationRuleManagementRequest).getBytes());
+
+        log.info("Response from AAM acquired.");
+
+        HashMap<String, FederationRule> responseMap = mapper.readValue(response, new TypeReference<HashMap<String, FederationRule>>() {
+        });
+        if (responseMap == null
+                || responseMap.get(federationId) == null) {
+            throw new SecurityException("Federation was not registered. Please, try again.");
+        }
+        log.info("Federation registered.");
+        return responseMap;
+    }
+
+    private static Connection getConnection(String rabbitHost, String rabbitUsername, String rabbitPassword) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(rabbitHost);
+        factory.setUsername(rabbitUsername);
+        factory.setPassword(rabbitPassword);
+        return factory.newConnection();
     }
 
 }
