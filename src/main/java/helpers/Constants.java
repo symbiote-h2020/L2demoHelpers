97aa5bd720cbae9c125952533969e71c9da1541b
Maksymilian Marcinowski <ksymilian@man.poznan.pl>
2017-09-19 10:36:33 +0200

    constants values added

--- src/main/java/helpers/Constants.java
+++ src/main/java/helpers/Constants.java
@@ -1,32 +1,32 @@
 package helpers;
 
 public class Constants {
-    public static final String AAMOwnerUsername = "";
-    public static final String AAMOwnerPassword = "";
+    public static final String AAMOwnerUsername = "AAMOwner";
+    public static final String AAMOwnerPassword = "AAMPassword";
 
-    public static final String platformOwnerUsername = "";
-    public static final String platformOwnerPassword = "";
-    public static final String federatedId = "";
-    public static final String recoveryMail = "";
+    public static final String platformOwnerUsername = "testPOusername";
+    public static final String platformOwnerPassword = "testPOpassword";
+    public static final String federatedId = "testFederatedId";
+    public static final String recoveryMail = "null@dev.null";
 
-    public static final String platformOwnerUsername2 = "";
-    public static final String platformOwnerPassword2 = "";
-    public static final String federatedId2 = "";
-    public static final String recoveryMail2 = "";
+    public static final String platformOwnerUsername2 = "testPOusername2";
+    public static final String platformOwnerPassword2 = "testPOpassword2";
+    public static final String federatedId2 = "federatedId2";
+    public static final String recoveryMail2 = "null2@dev.null";
 
-    public static final String platformId = "";
-    public static final String platformInstanceFriendlyName = "";
-    public static final String platformInterworkingInterfaceAddress = "";
+    public static final String platformId = "testPlatformId";
+    public static final String platformInstanceFriendlyName = "testPlatformInstanceFriendlyName";
+    public static final String platformInterworkingInterfaceAddress = "testPlatformInterworkingInterfaceAddress";
 
-    public static final String platformId2 = "";
-    public static final String platformInstanceFriendlyName2 = "";
-    public static final String platformInterworkingInterfaceAddress2 = "";
+    public static final String platformId2 = "testPlatformId2";
+    public static final String platformInstanceFriendlyName2 = "testPlatformInstanceFriendlyName2";
+    public static final String platformInterworkingInterfaceAddress2 = "testPlatformInterworkingInterfaceAddress2";
 
-    public static final String rabbitHost = "";
-    public static final String rabbitUsername = "";
-    public static final String rabbitPassword = "";
+    public static final String rabbitHost = "lccalhost";
+    public static final String rabbitUsername = "guest";
+    public static final String rabbitPassword = "guest";
 
-    public static final String userManagementRequestQueue = "";
-    public static final String platformManagementRequestQueue = "";
+    public static final String userManagementRequestQueue = "symbIoTe-AuthenticationAuthorizationManager-manage_user_request";
+    public static final String platformManagementRequestQueue = "symbIoTe-AuthenticationAuthorizationManager-manage_platform_request";
 
 }
